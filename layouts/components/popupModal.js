1930906159a0442442d86ffb111b7f35717ce4c8
NIKHIL SHARMA <nikhil722sharma@gmail.com>
2022-11-08 02:22:58 +0530

    zoomer fixes

--- layouts/components/popupModal.js
+++ layouts/components/popupModal.js
@@ -5,21 +5,27 @@ const Modal = ({ openModal, modalData, category }) => {
    
   useEffect(() => {
     document.body.style.overflow = 'hidden';
-    new Zoomist('#my-zoomist'),{
-        maxRatio: 1,
-        height: '60%',
-        // if you need silder
-        // if you need zoomer
-        zoomer: true,
-        draggable: true,
-        pinchable: true,
-        slider: {
-            el: null,
-            direction: 'vertical',
-            maxRatio: 2
-          },
-        // event
-    }
+    new Zoomist('#my-zoomist',{
+      src: 'data-zoomist-src',
+      fill: 'contain',
+      draggable: true,
+      wheelable: true,
+      pinchable: true,
+      // bounds: true,
+      zoomRatio: 0.1,
+      maxRatio: 4,
+      height: 'auto',
+      slider: {
+        el: null,
+        direction: 'vertical',
+        maxRatio: 2
+      },
+      zoomer: {
+        inEl: null,
+        outEl: null,
+        disableOnBounds: true
+      }
+    })
 
 //   const zoomScreen = document.querySelector(".productImageImg");
 //   let zoom = 1;
